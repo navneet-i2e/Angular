7919d2214fc2b87d9f9605223e838a8f6d13e9fd
navneet-i2e <navneet.anand@i2econsulting.com>
2024-10-23 09:48:14 +0530

    routes added

--- NavneetHomesForSale/src/main.ts
+++ NavneetHomesForSale/src/main.ts
@@ -1,5 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { provideRouter } from "@angular/router";
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+import routeConfig from "./app/routes";
+
+bootstrapApplication(AppComponent, { providers: [provideRouter(routeConfig)] }).catch(
+  (err) => console.error(err)
+);
